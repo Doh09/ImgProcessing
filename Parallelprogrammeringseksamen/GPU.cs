42dfb3c4f06ea51a981cd52409cccc072b2f9add
Doh09 <alexandreasen09@gmail.com>
2018-10-12 09:07:07 +0200

    Refactored building of List<String> for ManyImages testing in the ImgProcessor. Now uses a method to set length instead of hardcoding. Still has to all be the same image unless the method is called twice and AddRange is used though.

--- Parallelprogrammeringseksamen/GPU.cs
+++ Parallelprogrammeringseksamen/GPU.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Parallelprogrammeringseksamen
 {
@@ -9,5 +14,53 @@ namespace Parallelprogrammeringseksamen
     /// </summary>
     public class GPU
     {
+        public static void Run(int N, double[] a, double[] b)
+        {
+            Parallel.For(0, N, i => { a[i] += b[i]; });
+        }
+
+        public void ProcessImg_MapReduce_PLINQ(IEnumerable<Color> colors)
+        {
+            
+
+            //Stopwatch sw = new Stopwatch();
+            //sw.Restart();
+            //var candidates =
+            //    colors
+            //        //.AsParallel()
+            //        .GroupBy(color => color)
+            //        .Select(
+            //            intermediate => new
+            //            {
+            //                ColorKey = intermediate.Key,
+            //                Frequency = intermediate.Sum(c => 1)
+            //            })
+            //        .OrderBy(c => c.Frequency);
+            //sw.Stop();
+
+            //Console.WriteLine(sw.Elapsed);
+
+            //foreach (var result in candidates)
+            //{
+            //    var localR = result.ColorKey.R;
+            //    var localG = result.ColorKey.G;
+            //    var localB = result.ColorKey.B;
+            //    var colorToConsole = Color.FromArgb(localR, localG, localB);
+
+            //    Console.WriteLine(
+            //        $"Red: {localR} Green: {localG} Blue: {localB} Appeared: {result.Frequency}",
+            //       colorToConsole); //FromArgb(result.ColorKey.R, result.ColorKey.G, result.ColorKey.B));
+            //                        //Console.WriteLine($"Red: {result.ColorKey.R} Green: {result.ColorKey.G} Blue: {result.ColorKey.B} Appeared: {result.Frequency}");
+
+            //    Thread.Sleep(1000);
+            //}
+
+            //Console.ReadLine();
+
+
+            //.Select(foafGroup => new IDMultisetItem(foafGroup.Key,
+            //    foafGroup.Count()));
+            //return Multiset.MostNumerous(candidates, maxCandidates);
+        }
     }
 }
--- Parallelprogrammeringseksamen/ImgProcessor.cs
+++ Parallelprogrammeringseksamen/ImgProcessor.cs
@@ -16,27 +16,20 @@ namespace Parallelprogrammeringseksamen
     /// </summary>
     public class ImgProcessor
     {
-        public string BmpImgPath { get; set; } = @"TomAndJerry.bmp";
-        public List<string> BmpImgPaths { get; set; } = 
-            new List<string> {
-                @"TomAndJerry.bmp",@"TomAndJerry.bmp",@"TomAndJerry.bmp",
-                @"TomAndJerry.bmp",@"TomAndJerry.bmp",@"TomAndJerry.bmp",
-                @"TomAndJerry.bmp",@"TomAndJerry.bmp",@"TomAndJerry.bmp" };
-        public List<string> BmpImgPaths2 { get; set; } =
-    new List<string> {
-                @"./ImagesToWorkOn/TheWorld.bmp",@"./ImagesToWorkOn/TheWorld.bmp",@"./ImagesToWorkOn/TheWorld.bmp",
-                @"./ImagesToWorkOn/TheWorld.bmp",@"./ImagesToWorkOn/TheWorld.bmp",@"./ImagesToWorkOn/TheWorld.bmp",
-                @"./ImagesToWorkOn/TheWorld.bmp",@"./ImagesToWorkOn/TheWorld.bmp",@"./ImagesToWorkOn/TheWorld.bmp" };
-        public List<string> BmpImgPaths3 { get; set; } =
-    new List<string> {
-                @"f153065664.bmp",@"f153065664.bmp",@"f153065664.bmp",
-                @"f153065664.bmp",@"f153065664.bmp",@"f153065664.bmp",
-                @"f153065664.bmp",@"f153065664.bmp",@"f153065664.bmp" };
+        public string BmpImgPath { get; set; }
+        public List<string> BmpImgPaths { get; set; }
+        public List<string> BmpImgPaths2 { get; set; }
+        public List<string> BmpImgPaths3 { get; set; }
         public Bitmap Img { get; set; }
 
-        
+
         public void ProcessImage()
         {
+            BmpImgPath = @"TomAndJerry.bmp";
+            BmpImgPaths = GetStringAsList(@"TomAndJerry.bmp", 9);
+            BmpImgPaths2 = GetStringAsList(@"./ImagesToWorkOn/TheWorld.bmp", 9);
+            BmpImgPaths3 = GetStringAsList(@"f153065664.bmp", 9);
+            
             ColorLoader cl = new ColorLoader();
             //MultipleImages using tasks
             Stopwatch sw = new Stopwatch();
@@ -44,7 +37,7 @@ namespace Parallelprogrammeringseksamen
             List<Color> multipleImagesColors = cl.GetColourCollection_SequentialForLoop_ManyImages_UsingTasksAsync(BmpImgPaths).Result;
             sw.Stop();
             string asyncSwElapsed = sw.Elapsed.ToString();
-            Console.WriteLine("Multiple images - Async tasks - total time - "+ asyncSwElapsed);
+            Console.WriteLine("Multiple images - Async tasks - total time - " + asyncSwElapsed);
             sw.Restart();
             List<Color> multipleImagesColors2 = cl.GetColourCollection_SequentialForLoop_ManyImages(BmpImgPaths);
             sw.Stop();
@@ -63,8 +56,18 @@ namespace Parallelprogrammeringseksamen
             //colorParallel
             //ConcurrentBag<Color> colorParallel = cl.GetColourCollection_ParallelForLoop(BmpImgPath);
             var cpu = new CPU();
-            cpu.Initialize(multipleImagesColors,2);
+            cpu.Initialize(multipleImagesColors, 2);
 
+        }
+
+        public List<string> GetStringAsList(string stringToMultiply, int lengthOfList)
+        {
+            List<string> stringList = new List<string>();
+            for (int i = 0; i < lengthOfList; i++)
+            {
+                stringList.Add(stringToMultiply);
+            }
+            return stringList;
         }
 
         public void ProcessImg_LINQ()
