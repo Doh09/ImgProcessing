47cb913c686b964cde30c1439a830b534ccc76ea
Doh09 <alexandreasen09@gmail.com>
2018-10-23 11:55:37 +0200

    Web crawler now works for online directory with bmps.

--- Parallelprogrammeringseksamen/Program.cs
+++ Parallelprogrammeringseksamen/Program.cs
@@ -16,30 +16,37 @@ namespace Parallelprogrammeringseksamen
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Image Processor...");
-            //ImgProcessor imgp = new ImgProcessor();
-            //imgp.Initialize(new ColorLoader());
 
-            //var files = new List<string> { @"http://www.cs.tau.ac.il/~spike/images/ATM.bmp" };
 
-            var files = new List<string>();
-            string url = @"http://www.cs.tau.ac.il/~spike/images/";
+            var imgProcessingPipeline = new ImgProcessingPipeline();
+            imgProcessingPipeline.Initialize(FilePathsFromURL());
+
+            Console.WriteLine("Finished... Hit >ENTER< to quit...");
+            Console.ReadLine();
+        }
+
+        public static IList<string> FilePathsFromURL(string url = @"http://www.cs.tau.ac.il/~spike/images/") {
+            var files = new List<string>(); //https://html-agility-pack.net/knowledge-base/7760286/how-to-extract-full-url-with-htmlagilitypack---csharp
             HtmlWeb hw = new HtmlWeb();
             HtmlDocument doc = hw.Load(url);
             foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
             {
-                string s = link.ToString();
-                Console.WriteLine(s);
+                string onlineRelativeImgPath = "";
+                foreach (var a in link.Attributes["href"].Value)
+                {
+                    onlineRelativeImgPath += a;
+                }
+                if (onlineRelativeImgPath.EndsWith(".bmp"))
+                    files.Add(url + onlineRelativeImgPath);
             }
+            return files;
+        }
 
-            //var files = Directory.GetFiles(
-            //    @"..\..\..\ImagesToWorkOn", "*.bmp"
-            //    );
-
-            var imgProcessingPipeline = new ImgProcessingPipeline();
-            imgProcessingPipeline.Initialize(files);
-
-            Console.WriteLine("Finished... Hit >ENTER< to quit...");
-            Console.ReadLine();
+        public static string[] FilePathsFromDirectory(string directoryPath = @"..\..\..\ImagesToWorkOn") {
+            var files = Directory.GetFiles(
+                directoryPath, "*.bmp"
+                );
+            return files;
         }
     }
 }
